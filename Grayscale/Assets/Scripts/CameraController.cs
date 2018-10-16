5f170fe340e2974201398834804cf9ea2da4df39
eric tinoco <ertinoco@csumb.edu>
2018-10-16 16:10:11 -0700

    EricScene added: Basic movement(walk, jump, climb) Camera also follows

---
+++ Grayscale/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector3 offset;     // Holds a vector of a set distance between player and camera
+    public GameObject player;   // Object that will be following
+
+    void Start()
+    {
+        // Set the offset at start of game
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        // Set camera's position to players current position plus the offset
+        transform.position = player.transform.position + offset;
+    }
+}
