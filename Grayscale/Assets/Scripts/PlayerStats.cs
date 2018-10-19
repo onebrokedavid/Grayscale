9807291b18b8bbd8869a40e399c8b835739126ba
HakaruShokku <hakarushokku@gmail.com>
2018-10-18 21:43:44 -0700

    adding movement script

---
+++ Grayscale/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void takeDmg(int dmgAmt){
+		health -= dmgAmt;
+		checkDeathStatus();
+	}
+
+	void checkDeathStatus(){
+		if (health <= 0) {
+			//Destroy ();
+
+		}
+	}
+}
