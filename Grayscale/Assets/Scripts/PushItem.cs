d3ecac922cf6830e5c78884961b903ba0ed3c731
Nicholas Cerda <cerd6615@csumb.ad.csumb.edu>
2018-10-19 16:25:43 -0700

    add pushItem script

--- Grayscale/Assets/Scripts/PushItem.cs
+++ Grayscale/Assets/Scripts/PushItem.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PushItem : MonoBehaviour {
-
+    bool whileHolding = false;
     bool touchingObject = false;
+    bool reset = true;
+    float moveRight,moveLeft;
     GameObject collObj; //object colliding with player
+    GameObject collObjHolder;
     Vector3 playerPos, boxPos, boxDist;
 
 	// Use this for initialization
 	void Start () {
-        
+        moveRight = 1.1f;
 	}
 
 	
 	// Update is called once per frame
 	void Update () {
         PickUpObject(touchingObject);
+        if (whileHolding)
+        {
+            playerPos = transform.position;
+            boxPos = collObjHolder.transform.position;
+            boxDist = playerPos - collObjHolder.transform.position;
+            if (Input.GetKey("a") && playerPos.x >boxPos.x)
+                collObjHolder.transform.position = new Vector3((playerPos.x - moveRight), (playerPos.y -boxDist.y), collObj.transform.position.z);
+            else if (Input.GetKey("d") && playerPos.x <boxPos.x)
+            {
+                collObjHolder.transform.position = new Vector3((playerPos.x + moveRight), (playerPos.y -boxDist.y ), collObj.transform.position.z);
+            }
+            if (Input.GetKeyUp("f"))
+            {
+                whileHolding = false;
+                reset = true;
+            }
+        }
 	}
 
 
@@ -27,6 +47,11 @@ public class PushItem : MonoBehaviour {
             Debug.Log("Player touching.");
             touchingObject = true;
             collObj = collision.gameObject;
+            if (reset)
+            {
+                collObjHolder = collObj;
+                reset = false;
+            }
         }
         else
         {
@@ -50,9 +75,9 @@ public class PushItem : MonoBehaviour {
             if (Input.GetKey("f"))
             {
                 Debug.Log("Holding object");
-
+                whileHolding = true;
                 //drag object
-                collObj.transform.position = new Vector3((playerPos - boxDist).x, (playerPos - boxDist).y);
+                collObj.transform.position = new Vector3((playerPos.x - boxDist.x), (playerPos.y - boxDist.y));
 
             }
             if(Input.GetKeyUp("f"))
