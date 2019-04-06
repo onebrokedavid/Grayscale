c084ef63aada8e8dfac9541a97e8212051294954
Jessica Ubaldo <jubaldo@csumb.edu>
2019-04-05 17:10:51 -0700

    Hidden

---
+++ Grayscale/Assets/ChangeGround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGround : MonoBehaviour {
+    public GameObject gm;
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnCollisionStay");
+        Debug.Log(collision.collider.tag);
+        if (collision.collider.tag == "moveableObject")
+        {
+            Debug.Log("Entered Sound tag if-statement");
+            //this.SetActive(false);
+        }
+    }
+}
